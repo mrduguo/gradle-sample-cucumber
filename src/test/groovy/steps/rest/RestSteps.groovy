d052a2671e155d63da4da48bc2651cd0b5e6933a
Guo Du <mrduguo@gmail.com>
2016-04-07 22:14:54 +0100

    optimized import

--- src/test/groovy/steps/rest/RestSteps.groovy
+++ src/test/groovy/steps/rest/RestSteps.groovy
@@ -5,8 +5,6 @@ import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
 
-import static org.hamcrest.core.IsEqual.equalTo
-
 class RestSteps {
 
     def request = RestAssured.with()
